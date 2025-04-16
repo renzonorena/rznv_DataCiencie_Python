b3f208bb0688bb953f880b897508f226d3dd2983
renzonorena <rezanova2302@gmail.com>
2025-04-15 22:30:46 -0500

    primera version

---
+++ estructuraDatos/estructura_ejemplos.py
@@ -0,0 +1,27 @@
+# estrcutura de datos dinamicos
+# Listas
+# Tuplas
+# Conjuntos
+# Dicionarios
+
+'''''''''
+persona = {
+    "nombre": "Anita",
+    "edad" : 29,
+    "ciudad" : "Lima"
+}
+
+print("======================== impresiones =========================")
+print(persona["nombre"])
+
+persona["profesion"] = "Ingeniera Industrial"
+'''''''''
+
+# ejemplo de conjuntos
+print("======================== impresiones =========================")
+numeros_1 = {1,2,3,4,5,6}
+numeros_2 = {4,5,6,7,8,9,10}
+union = numeros_1 | numeros_2
+
+interseccion  = numeros_1 & numeros_2
+print(interseccion)
